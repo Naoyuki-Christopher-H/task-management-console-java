2e11650214b423913531a8fae6bd51bff857948d
Naoyuki-Christopher-H <naoyuki.christopher.higaki@outlook.com>
2026-02-09 14:00:46 +0200

    Add project files.

---
+++ src/main/java/Solution/CommandParser.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Solution;
+
+/**
+ *
+ * @author naoyu
+ */
+public class CommandParser
+{
+    
+}
---
+++ src/main/java/Solution/TaskManagementConsoleJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Solution;
+
+/**
+ *
+ * @author naoyu
+ */
+public class TaskManagementConsoleJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
